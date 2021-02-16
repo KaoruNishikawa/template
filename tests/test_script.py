dc88fcc07cf96bf2cab216378f1721b513ceed72
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-02-17 01:09:50 +0900

    make test function

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,4 +1,5 @@
 import script
 
-assert script.__author__ == "Kaoru Nishikawa"
-assert script.__version__ == "0.1.0"
+def test_metadata():
+    assert script.__author__ == "Kaoru Nishikawa"
+    assert script.__version__ == "0.1.0"
