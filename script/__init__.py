fc420e9ea5f18f157fa22b8eb6b7f08f6032cafe
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-02-17 01:01:25 +0900

    add test

--- script/__init__.py
+++ script/__init__.py
@@ -1,2 +1,3 @@
 __version__ = "0.1.0"
-__author__ = "KaoruNishikawa"
+__author__ = "Kaoru Nishikawa"
+
---
+++ tests/test_script.py
@@ -0,0 +1,5 @@
+import script
+
+assert script.__author__ == "Kaoru Nishikawa"
+assert script.__version__ == "0.1.0"
+
