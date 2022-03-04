11156a6ae9e07a5c1ebeeaf6bb8ff1f8dc83dc1c
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-03-04 15:02:37 +0900

    Change docs style

--- docs/conf.py
+++ docs/conf.py
@@ -1,43 +1,60 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../n_const"))
+import PACKAGENAME
 
 
 # -- Project information -----------------------------------------------------
 
 project = "PACKAGENAME"
-copyright = "2021, CREDIT"
-author = "CREDIT"
-
-# The full version, including alpha/beta/rc tags
-release = "0.1.0"
+copyright = "YEAR, AUTHOR"
+author = "AUTHOR"
+release = version = PACKAGENAME.__version__
 
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
     "m2r2",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 templates_path = ["_templates"]
-
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+autosummary_generate = True
+add_module_names = False
+autodoc_member_order = "bysource"
+autodoc_typehints_format = "short"
+autodoc_typehints = "description"
+napoleon_use_ivar = True
 
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-    "github_url": "https://github.com/USERNAME/PACKAGENAME/",
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/USERNAME/PACKAGENAME",
+            "icon": "fab fa-github-square",
+            "type": "fontawesome",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/PACKAGENAME/",
+            "icon": "fas fa-cubes",
+            "type": "fontawesome",
+        },
+    ],
+    "navbar_start": ["navbar-logo"],
 }
-html_logo = "_static/logo.svg"
+# html_logo = "_static/logo.svg"
+html_favicon = "https://avatars.githubusercontent.com/u/20414019?s=400&u=0e47c7d5efc6cf27086c8cfcdb6fd5c757926043&v=4"  # noqa: E501
 html_sidebars = {
     "**": [
+        "version",
+        "search-field.html",
         "sidebar-nav-bs.html",
-        "sidebar-search-bs.html",
     ],
 }
 
